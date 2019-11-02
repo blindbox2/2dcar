441dde8c57eb0293eb6b44386d260809d6623a69
Rick de Harder <rickdeharder@hotmail.com>
2019-11-02 22:14:23 +0100

    0.1

--- track.py
+++ track.py
@@ -1,5 +1,5 @@
 import pygame
-from math import degrees, sin, cos, radians
+from math import sin, cos, radians
 
 pygame.init()
 
@@ -8,8 +8,10 @@ class color:  # color variables
     Black = (0, 0, 0)
     Red = (255, 0, 0)
     White = (255, 255, 255)
+    Yellow = (255, 255, 0)
     Green = (0, 255, 0)
     Blue = (0, 0, 255)
+    colors = [Black, Red, Yellow, Green, Blue]
 
 
 class main:  # general setup variables
@@ -19,18 +21,16 @@ class main:  # general setup variables
     done = False
 
 
-class rect(object):
+class track(object):
     def __init__(self):
         self.width = 60
         self.height = 90
-        self.degree = degrees(90)
-        self.leftTop = [400, 400]
-        self.position = position(self.leftTop, self.width, self.height)
-        self.middle = middle(self.position, self.width, self.height)
+        self.start = [400, 400]
+        self.toDraw = []
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, color.Blue, self.position, 2)
-        pygame.draw.polygon(screen, color.Red, next(self.position, self.width, self.height, 45), 2)
+        for rectangle in self.toDraw:
+            pygame.draw.polygon(screen, color.colors[self.toDraw.index(rectangle)], rectangle)
 
 
 def draw(screen):  # draws all elements and updates the display
@@ -39,56 +39,86 @@ def draw(screen):  # draws all elements and updates the display
     pygame.display.update()
 
 
-def position(start, width, height):
+def straight(start, direction, length):
+    rectangles = [position(start)]
+
+    for rectangle in range(length):
+        if direction == 'horizontal':
+            start = rectangles[rectangle][1]
+        else:
+            start = [rectangles[rectangle][0][0], rectangles[rectangle][0][1] - track.height]
+        rectangles.append(position(start))
+
+    return rectangles
+
+
+def corner(start, degree, direction):
+    rectangles = [position(start)]
+    numberOfRectangles = int(degree // 22.5)
+
+    for rectangle in range(numberOfRectangles):
+        if(direction == 'right'):
+            degree = 22.5
+            corners = rotate(rectangles[rectangle], degree)
+            difx = rectangles[rectangle][0][0] - corners[3][0]
+            dify = corners[3][1] - rectangles[rectangle][0][1]
+
+            for corner in corners:
+                corner[0] += difx
+                corner[1] -= dify
+
+        else:
+            degree = -22.5
+            corners = rotate(rectangles[rectangle], degree)
+            difx = corners[2][0] - rectangles[rectangle][1][0]
+            dify = corners[2][1] - rectangles[rectangle][1][1]
+
+            for corner in corners:
+                corner[0] -= difx
+                corner[1] -= dify
+
+        rectangles.append(corners)
+    return rectangles
+
+
+def position(start):
     position = []
     position.append(start)
-    position.append([start[0] + width, start[1]])
-    position.append([start[0] + width, start[1] + height])
-    position.append([start[0], start[1] + height])
+    position.append([start[0] + track.width, start[1]])
+    position.append([start[0] + track.width, start[1] + track.height])
+    position.append([start[0], start[1] + track.height])
     return position
-
-
-def next(a, width, height, degree):
-    start = a[0]
-    test = position(start, width, height)
-    rot = rotate(test, degree)
-    difx = abs(rot[3][0] - test[0][0])
-    dify = abs(rot[3][1] - test[0][1])
-
-    for i in range(4):
-        rot[i][0] += difx
-        rot[i][1] -= dify
-
-    return rot
-
-
-def middle(position, width, height):
-    x = position[0][0] + width // 2
-    y = position[0][1] + height // 2
-    middle = [x, y]
-    return middle
-
-
-def relativePosition(coordinates, middle):
-    relativeCoordinates = []
-    for coordinate in range(len(coordinates)):
-        relativeCoordinates.append([coordinates[coordinate][0] - middle[0], coordinates[coordinate][1] - middle[1]])
-    return relativeCoordinates
 
 
 def rotate(position, degree):
     degree = radians(degree)
+    centre = middle(position)
     pos = []
-    relativeCoordinates = relativePosition(position, track.middle)
-    for corner in range(len(relativeCoordinates)):
-        x = track.middle[0] + (cos(degree) * relativeCoordinates[corner][0] + -sin(degree) * relativeCoordinates[corner][1])
-        y = track.middle[1] + (sin(degree) * relativeCoordinates[corner][0] + cos(degree) * relativeCoordinates[corner][1])
+    relativeCoordinates = relativePosition(position, centre)
+    for i in range(len(relativeCoordinates)):
+        x = centre[0] + (cos(degree) * relativeCoordinates[i][0] + -sin(degree) * relativeCoordinates[i][1])
+        y = centre[1] + (sin(degree) * relativeCoordinates[i][0] + cos(degree) * relativeCoordinates[i][1])
         pos.append([x, y])
     return pos
 
 
-track = rect()
+def middle(position):
+    x = position[0][0] + track.width // 2
+    y = position[0][1] + track.height // 2
+    middle = [x, y]
+    return middle
+
+
+def relativePosition(coordinates, centre):
+    relativeCoordinates = []
+    for coordinate in range(len(coordinates)):
+        relativeCoordinates.append([coordinates[coordinate][0] - centre[0], coordinates[coordinate][1] - centre[1]])
+    return relativeCoordinates
+
+
 clock = pygame.time.Clock()
+track = track()
+track.toDraw = straight(track.start, 'vertical', 3)
 while not main.done:
     dt = clock.tick(10)
 
